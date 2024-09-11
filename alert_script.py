7c314b302410582ec5ee2ff20e39c2491bf68c40
Kevin Jacob <155554880+Kjacob10@users.noreply.github.com>
2024-09-11 16:00:54 +0530

    Update alert_script.py

--- alert_script.py
+++ alert_script.py
@@ -1,16 +1,16 @@
 from twilio.rest import Client
 
 def send_call_alert(phone_number, message):
-    account_sid = 'ACf4b1ffbd2b85230d94f416aaf9fd36c0'
-    auth_token = '7f65af01c3f09b0baf0b48ea7e6403dd'
+    account_sid = 'your_account_sid'
+    auth_token = 'your_auth_token'
     client = Client(account_sid, auth_token)
 
     call = client.calls.create(
         to=phone_number,
-        from_='+12084860252',
+        from_='your_twilio_number',
         twiml=f'<Response><Say>{"This is an alert message"}</Say></Response>'
     )
     print(f"Call sent to {phone_number}, SID: {call.sid}")
 
 if __name__ == "__main__":
-    send_call_alert('+919359461477', 'Server is down! Please check immediately.')
+    send_call_alert('verified_phone_number_to_recieve_message', 'Server is down! Please check immediately.')
